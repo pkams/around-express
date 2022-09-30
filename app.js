ad055a151eece4f313a0fa41575d3d80b8050e57
Patrick Souza <patrick_ams@hotmail.com>
2022-09-29 23:07:04 -0300

    update readme

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cards = require("./routes/cards"); // importando o roteador
-const users = require("./routes/users"); // importando o roteador
+const express = require('express');
+const cards = require('./routes/cards'); // importando o roteador
+const users = require('./routes/users'); // importando o roteador
 // escute o port 3000
 const { PORT = 3000 } = process.env;
 
@@ -9,14 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", cards, users);
+app.use('/', cards, users);
 
-app.get("/", (req, res) => {
-  res.status(404).send("Frontend ainda não conectado.");
+app.get('/', (req, res) => {
+  res.status(404).send('Frontend ainda não conectado.');
 });
 
-app.get("*", (req, res) => {
-  res.status(404).send({ message: "A solicitação não foi encontrada" });
+app.get('*', (req, res) => {
+  res.status(404).send({ message: 'A solicitação não foi encontrada' });
 });
 
 app.listen(PORT, () => {
